270c81cb04f686455e198afc4eec63bd592b7d32
MaySoMusician <maysomusician@gmail.com>
2019-06-15 00:18:55 +0900

    Fix nested Promise.all() problem in commands/banlist.js

--- commands/banlist.js
+++ commands/banlist.js
@@ -1,15 +1,16 @@
 exports.run = async (client, message, args) => {
   const formatUserData = userId => client.fetchUser(userId).then(u => `${u.username} (${u.id})`),
-        usersBannedIndefinitely = Promise.all(client.userService.usersIndefinitelyBanned.map(id => formatUserData(id))),
-        usersBannedTemporarily = Promise.all(client.userService.usersTemporarilyBanned.map(id => formatUserData(id)));
-  await Promise.all(usersBannedIndefinitely.concat(usersBannedTemporarily));
-
-  const results = `Users that have been banned indefinitely
-${usersBannedIndefinitely.join('\n')}
+        [bannedIndef, bannedTemp] = await Promise.all([
+          Promise.all(client.userService.usersIndefinitelyBanned.map(id => formatUserData(id))),
+          Promise.all(client.userService.usersTemporarilyBanned.map(id => formatUserData(id))),
+        ]),
+        results = `Users that have been banned indefinitely
+${bannedIndef.join('\n')}
 
 Users that have been banned temporarily
-${usersBannedTemporarily.join('\n')}
+${bannedTemp.join('\n')}
 `;
+
   await message.channel.send('```\n' + results + '```');
 };
 
